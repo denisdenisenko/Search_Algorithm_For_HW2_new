665cf4f33c1b19757c6e0b89506d2f5847947f16
Denis <denis.denisenko@gmail.com>
2021-01-04 20:40:22 +0200

    added test cases

---
+++ Search_Algorithm_For_HW2Tests/SortAndSearchTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Search_Algorithm_For_HW2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search_Algorithm_For_HW2.Tests
+{
+    [TestClass()]
+    public class SortAndSearchTests
+    {
+        [TestMethod()]
+        public void SortAlgorithmTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FirstSearchTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void helpingSearchTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SecondSearchTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ThirdSearchTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DisplayElementsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
